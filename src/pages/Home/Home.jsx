72e3d00d1bbe5a803ba5c5b7ac00401df4aaf88e
ronniepoon123 <ronniepoon_rp@hotmail.com>
2026-07-23 20:32:45 +0800

    Initial React project setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <main className="home">
+      <h1 className="logo">Concoctail</h1>
+
+      <div className="tagline">
+        <h2>Discover.</h2>
+        <h2>Build.</h2>
+        <h2>Mix.</h2>
+      </div>
+
+      <p className="subtitle">
+        Cocktails made simple.
+      </p>
+    </main>
+  );
+}
+
+export default Home;
